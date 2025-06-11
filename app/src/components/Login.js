af6618846e427945d6bea51cc838f2afee876e3b
Abhishekvk04 <vkabhishek04@gmail.com>
2025-06-11 21:31:11 +0530

    corrected the login error

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'hhttps://krishijal.onrender.com';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://krishijal.onrender.com';
 
 const Login = ({ setUser }) => {
   const navigate = useNavigate();
